81254245ed419323c2fe8e2b649e40344f0a57b2
Terence-lr <trichardson0826@gmail.com>
2024-11-01 20:10:50 -0400

    course info & assingment group

---
+++ script.js
@@ -0,0 +1,33 @@
+// Course Information
+const CourseInfo = {
+  id: 678,
+  name: 'Advanced JavaScript',
+}
+
+// Assignment Group
+const AssignmentGroup = {
+  id: 98765,
+  name: 'JavaScript Mastery',
+  course_id: 678,
+  group_weight: 30,
+  assignments: [
+    {
+      id: 101,
+      name: 'Master Loops',
+      due_at: '2024-06-15',
+      points_possible: 100,
+    },
+    {
+      id: 102,
+      name: 'DOM Manipulation',
+      due_at: '2024-07-20',
+      points_possible: 200,
+    },
+    {
+      id: 103,
+      name: 'Async Programming',
+      due_at: '2024-12-01',
+      points_possible: 300,
+    },
+  ],
+}
